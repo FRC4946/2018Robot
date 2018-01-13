654cfeed45e2080377ecd717b56415c73b4f3c8c
Jacob <Jacob@169.254.60.143>
2018-01-13 13:49:27 -0500

    Removed ExampleCommand And Subsystem

--- src/org/usfirst/frc/team4946/robot/Robot.java
+++ src/org/usfirst/frc/team4946/robot/Robot.java
@@ -41,9 +41,9 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void robotInit() {
 		m_oi = new OI();
-		m_chooser.addDefault("Default Auto", new ExampleCommand());
+		//m_chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
-		SmartDashboard.putData("Auto mode", m_chooser);
+		//SmartDashboard.putData("Auto mode", m_chooser);
 	}
 
 	/**
