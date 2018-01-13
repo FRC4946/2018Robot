877168e9f58aa97c30c2cd43e376ff16bb46ff2a
Zhengmao <Zhengmao@10.31.200.158>
2018-01-13 10:31:45 -0500

    Accidental conflicts fixed.

--- src/org/usfirst/frc/team4946/robot/commands/DriveWithJoysticks.java
+++ src/org/usfirst/frc/team4946/robot/commands/DriveWithJoysticks.java
@@ -15,7 +15,7 @@ public class DriveWithJoysticks extends Command {
     public DriveWithJoysticks() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	requires(Robot.k_DriveTrain);
+    	requires(Robot.k_DriveTrain); //tHiS iS aNoThEr coummetn
     }
 
     // Called just before this Command runs the first time
@@ -24,7 +24,9 @@ public class DriveWithJoysticks extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.kDriveTrain.driveRobot(OI.k_joy1.getRawAxis(1)/5, OI.k_joy1.getRawAxis(0)/5);
+
+    	Robot.k_DriveTrain.driveRobot(OI.k_joy1.getRawAxis(0)*0.5, OI.k_joy1.getRawAxis(1)*0.5);
+
     }
 
     // Make this return true when this Command no longer needs to run execute()
