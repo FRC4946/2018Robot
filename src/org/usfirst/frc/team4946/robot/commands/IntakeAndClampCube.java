78f4d9dd79f10c0856aafea9947919e2398da3ad
Jacob <Jacob@169.254.60.143>
2018-01-24 17:42:51 -0500

    Added intakeAndClampCube Command Group

---
+++ src/org/usfirst/frc/team4946/robot/commands/IntakeAndClampCube.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team4946.robot.commands;
+
+import org.usfirst.frc.team4946.robot.Robot;
+import org.usfirst.frc.team4946.robot.commands.clamp.ChangeClamp;
+import org.usfirst.frc.team4946.robot.commands.intake.IntakeUntilCube;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class IntakeAndClampCube extends CommandGroup {
+
+    public IntakeAndClampCube() {
+    	addSequential(new ChangeClamp(false));
+    	addSequential(new IntakeUntilCube(1.0)); //The 1.0 argument may need to be reversed
+    	addSequential(new ChangeClamp(true));
+    }
+}
