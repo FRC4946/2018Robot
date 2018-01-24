5941df16723b9b9dc8f5a3e2add186c78e185c92
Jacob <Jacob@169.254.60.143>
2018-01-24 16:14:25 -0500

    Added UpperOutpput Subsystem and RunOutput Command

--- src/org/usfirst/frc/team4946/robot/subsystems/UpperOutputSubsystem.java
+++ src/org/usfirst/frc/team4946/robot/subsystems/UpperOutputSubsystem.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
-public class OutputMechanism extends Subsystem {
+public class UpperOutputSubsystem extends Subsystem {
 	
 	private WPI_TalonSRX m_leftMotor = new WPI_TalonSRX(RobotMap.CAN_INTAKE_LEFTMOTOR);
 	private WPI_TalonSRX m_rightMotor = new WPI_TalonSRX(RobotMap.CAN_INTAKE_LEFTMOTOR);
@@ -22,13 +22,18 @@ public class OutputMechanism extends Subsystem {
 		m_rightMotor.set(0); //Equivalent to .disable
 	}
 	
-	public void spin(double d_speed) { //Takes values from -1.0 to 1.0. Spins the motors, positive is forwards, negative is backwards (Maybe, or it might be the other way around).
+	public void set(double d_speed) { //Takes values from -1.0 to 1.0. Spins the motors, positive is forwards, negative is backwards (Maybe, or it might be the other way around).
 		m_leftMotor.set(d_speed);
 		m_rightMotor.set(-1.0*d_speed);		
 	}
 	
 	public boolean getHasCube() {
 		return m_cubeSwitch.get();
+	}
+	
+	public void stop() {
+		m_leftMotor.set(0);
+		m_rightMotor.set(0);
 	}
 
     public void initDefaultCommand() {
