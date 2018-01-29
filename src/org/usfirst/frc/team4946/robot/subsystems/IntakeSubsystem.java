3230f7ae08242227183450f74ece07aeace4790f
Jacob <Jacob@169.254.60.143>
2018-01-29 16:50:42 -0500

    Added JavaDocs tto IntakeSubsystem and OutputSubsystem

--- src/org/usfirst/frc/team4946/robot/subsystems/IntakeSubsystem.java
+++ src/org/usfirst/frc/team4946/robot/subsystems/IntakeSubsystem.java
@@ -23,19 +23,34 @@ public class IntakeSubsystem extends Subsystem {
 
 	}
 
-	// TODO: Add javadoc
+	/** Manually sets the speed of the motors.
+	 * 
+	 * @param d_speed The fraction of the motor's maximum speed the motors are to spin at. Ranges between -1.0 and 1.0
+	 * 
+	 * 
+	 */
 	public void set(double d_speed) { // Takes values from -1.0 to 1.0. Spins the motors, positive is forwards,
 										// negative is backwards (Maybe, or it might be the other way around).
 		m_leftMotor.set(d_speed);
 		m_rightMotor.set(-1.0 * d_speed);
 	}
 
-	// TODO: Add javadoc
+	/**
+	 * Sets the speed of both motors to 0.
+	 * 
+	 * 
+	 * 
+	 */
 	public void stop() { // Disables both motors
 		m_leftMotor.set(0);
 		m_rightMotor.set(0); // Equivalent to .disable
 	}
-
+	
+	/**
+	 * Finds out if the cube is in the intake subsystem.
+	 * 
+	 * @return whether the cube is in the intake subsystem
+	 */
 	public boolean getHasCube() {
 		return m_cubeSwitch.get();
 	}
