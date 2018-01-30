35637c5f758e948375d84b973d2af2e0b9e89eab
Allison <Allison@10.31.192.212>
2018-01-30 15:17:52 -0500

    Fixed a few naming errors

---
+++ src/org/usfirst/frc/team4946/robot/commands/DriveTrainGearShift.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team4946.robot.commands;
+
+import org.usfirst.frc.team4946.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveTrainGearShift extends Command {
+
+	boolean position;
+	
+    public DriveTrainGearShift(boolean position) {
+    	requires(Robot.transmissionSubsystem);
+        
+    	this.position = position;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
