c335d5104cbc565ecd7ecf8567c4352536351235
Zhengmao <Zhengmao@10.31.202.194>
2018-02-03 14:00:13 -0500

    DriveStraight

--- src/org/usfirst/frc/team4946/robot/commands/drivetrain/DriveStraight.java
+++ src/org/usfirst/frc/team4946/robot/commands/drivetrain/DriveStraight.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4946.robot.commands.drivetrain;
 
+import org.usfirst.frc.team4946.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -7,30 +9,41 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class DriveStraight extends Command {
 
-    public DriveStraight() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+	double m_speed; //in case the turning is too abrupt
+	double m_distanceToGo;
+	double m_distanceInit;
+	
+    public DriveStraight(double distance, double speed) {
+        
+    	requires(Robot.driveTrainSubsystem);
+    	m_distanceToGo = distance;
+    	m_speed = speed;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	m_distanceInit = Robot.driveTrainSubsystem.getEncoderDistance();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
+    	Robot.driveTrainSubsystem.arcadeDrive(m_speed, 0.0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return ((m_distanceInit + m_distanceToGo) - Robot.driveTrainSubsystem.getEncoderDistance()) <= 1.0;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.driveTrainSubsystem.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
