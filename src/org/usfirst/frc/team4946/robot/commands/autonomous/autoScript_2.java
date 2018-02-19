22a89599a3100352e66589478970d11c96827365
Jarred <Jarred@192.168.17.19>
2018-02-19 13:47:50 -0500

    Auto changes

--- src/org/usfirst/frc/team4946/robot/commands/autonomous/autoScript_2.java
+++ src/org/usfirst/frc/team4946/robot/commands/autonomous/autoScript_2.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team4946.robot.commands.autonomous;
 import org.usfirst.frc.team4946.robot.RobotConstants;
 import org.usfirst.frc.team4946.robot.commands.drivetrain.auto.DriveStraightPID;
 import org.usfirst.frc.team4946.robot.commands.drivetrain.auto.TurnPID;
+import org.usfirst.frc.team4946.robot.commands.elevator.MoveToHeight;
 import org.usfirst.frc.team4946.robot.commands.intake.IntakeWithTimer;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -41,6 +42,8 @@ public class autoScript_2 extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
+    	
+    	addSequential(new MoveToHeight(elevatorMinHeight));
     	
     	if(startPosition == 1)	    	//Start from scale left
     	{
