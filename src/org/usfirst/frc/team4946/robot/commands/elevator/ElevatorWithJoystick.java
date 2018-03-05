c1c7b98e191e5faf4ce2c76832085b794a8ee442
Matthew Reynolds <rey.mtr@gmail.com>
2018-03-05 08:41:30 -0500

    Commented and removed unused autonomous code

--- src/org/usfirst/frc/team4946/robot/commands/elevator/ElevatorWithJoystick.java
+++ src/org/usfirst/frc/team4946/robot/commands/elevator/ElevatorWithJoystick.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team4946.robot.commands.elevator;
 import org.usfirst.frc.team4946.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
